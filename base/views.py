4655c81242b7933f122b7f6c03d9199af6f5fb9a
devjoaoroberto <devjoaoroberto@gmail.com>
2024-05-10 21:12:09 -0300

    Create a api

--- base/views.py
+++ base/views.py
@@ -64,8 +64,6 @@ def home(request):
     # q = ''
     # if request.GET.get('q') != None:
     #     q = request.GET.get('q')
-    all_rooms_count = Room.objects.all().count()
-
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     
     rooms = Room.objects.filter(
@@ -74,7 +72,7 @@ def home(request):
     )
 
     room_count = rooms.count()
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
     context = {
@@ -82,7 +80,6 @@ def home(request):
         'topics': topics,
         'room_count': room_count,
         'room_messages': room_messages,
-        'all_rooms_count': all_rooms_count,
     }
     return render(request, 'base/home.html', context)
 
@@ -199,3 +196,12 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
         
     return render(request, 'base/update_user.html', {'form': form})
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
